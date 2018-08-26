3c38618ba7e0fee157122ac840de5d5d2f96dff7
megangodwin <megan.godwin@my.jcu.edu.au>
2018-08-26 13:11:22 +1000

    initial commit

---
+++ src/Fixture.java
@@ -0,0 +1,2 @@
+public class Fixture {
+}
---
+++ src/SmartHouseSimulator.java
@@ -0,0 +1,8 @@
+public class SmartHouseSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Smart House Simulator. Select an option to continue");
+
+
+    }
+}
